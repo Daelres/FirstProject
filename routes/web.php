9c4d6838344f15775098edb90f361f20e8dcf9c5
Daelres <daelres10@gmail.com>
2025-09-04 10:56:09 -0500

    Variables y tipos de variaables

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo"************** Variables y tipos de variables **************<br>";
+    $name = "Daniel Restrepo";
+    $age = rand(18,40);
+    $height = 1.86;
+    $islogin = true;
+
+    echo "Mi nombre es $name, tengo $age años, mido $height metros";
+
 });
